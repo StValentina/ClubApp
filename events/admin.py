6d73ba6fcb1927bab9dd6b471550f625034ca92f
valentina.st999@gmail.com <valentina.st999@gmail.com>
2025-07-11 20:20:56 +0300

    Добавяне на админ

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from events.models import Event
+
+
 # Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'club', 'date', 'location', 'creator')
+    list_filter = ('date', 'club',)
+    search_fields = ('title', 'location')
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from posts.models import Post, Comment
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'club', 'author', 'created_on')
+    list_filter = ('club', 'created_on')
+    search_fields = ('title', 'content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('author__username', 'text')
